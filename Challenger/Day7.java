9bc8b8bb8ff5945251c1136b6d75501da12240f6
Kayo Leanndro <kayoleanndro2@gmail.com>
2024-12-11 09:52:31 -0300

    Dia 7

--- Challenger/Day7.java
+++ Challenger/Day7.java
@@ -5,9 +5,22 @@ public class Day7 {
         Scanner sc = new Scanner(System.in);
         String string = sc.nextLine();
         
-        String[] stringSlitep = string.split(string);
+        //String cortada
+        String[] stringSlitep = string.split("");
+        //Variavel de string que vai ser invertida  
+        String[] inversedString = new String[stringSlitep.length];
+        //Percorrer o tamanho total do ArrayString 
         for(int i = 0; i < stringSlitep.length; i++){
-            System.out.println("dsd" + i);
+            for(int y = stringSlitep.length - 1; y >= 0; y--){ 
+                inversedString[i] = stringSlitep[y];
+                i++;
+            }
         }
+
+        for(int i = 0; i < stringSlitep.length; i++ ){
+            System.out.print(inversedString[i]);
+        }
+
     }
 }
+
