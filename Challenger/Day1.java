7cdb86ce850b22c2a08229708a552c18f645989f
Kayo Leanndro <kayoleanndro2@gmail.com>
2025-01-17 23:45:51 -0300

    day 13 e logicas

--- Challenger/Day1.java
+++ Challenger/Day1.java
@@ -1,5 +1,11 @@
-public class Day1{
+public class Day1 {
     public static void main(String[] args) {
+        // Logica utilizada:
+        // Utilizando a biblioteca padrão do java, O System.out acessa a saida padrão
+        // do sistema que é gerenciada pela JVM
+
+        // E o println é um metodo da classe PrintStream que escreve o texto.
+
         System.out.println("Olá, Mundo!");
     }
 }
---
+++ Challenger/Day13.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Day13 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite sua palavra para verificarmos se é um palindromo ou não");
+        String palavra = sc.nextLine();
+        System.out.println(palavraEPalindromo(palavra));
+    }
+
+    public static String palavraEPalindromo(String palavra) {
+        // Logica utilizada:
+
+        // Primeiramente pegamos a palavra, invertemos ela e atribuimos a palavra
+        // invertida
+        // a variavel resultado, depois disso verificamos se a palavra resultado é igual
+        // a
+        // palavra que foi passada como parametro.
+
+        String resultado = "";
+        for (int i = palavra.length() - 1; i >= 0; i--) {
+            resultado += palavra.charAt(i);
+        }
+        if (resultado.equals(palavra)) {
+            return "Palindromo";
+        }
+        return "Nao Palindromo";
+
+    }
+}
